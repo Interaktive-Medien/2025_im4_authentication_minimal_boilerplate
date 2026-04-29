a83c04d3626b1f344deef2af51f023971406f993
Nick Schneeberger <nick.schneeberger@icloud.com>
2026-04-29 22:33:58 +0200

    remove hidden class in protected

--- js/protected.js
+++ js/protected.js
@@ -12,10 +12,8 @@ async function checkAuth() {
     const result = await response.json();
 
     // Display user data in the protected content div
-    const protectedContent = document.getElementById("protectedContent");
     document.getElementById("userEmail").textContent = result.email;
     document.getElementById("userId").textContent = result.user_id;
-    protectedContent.hidden = false;
 
     return true;
   } catch (error) {
